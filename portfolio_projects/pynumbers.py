dc1d4ed9a0361ba0c32fdbc35c49f2b494e3d381
AFaurot <aaron.faurot@csuglobal.edu>
2025-05-18 23:24:28 -0600

    adding python scripts for portfolio milestones

---
+++ portfolio_projects/pynumbers.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+file = open("file4.txt", "w")
+start_time = time.time()
+for i in range(1000000):
+    random_number = random.randint(0,100000)
+    file.write(str(random_number))
+    file.write("\n")
+file.close()
+end_time = time.time()
+execution_time = end_time-start_time
+print(f"Execution took: {execution_time} seconds")
