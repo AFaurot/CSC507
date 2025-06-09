019567034020095775618af3b10aaf973b530cf6
AFaurot <aaron.faurot@csuglobal.edu>
2025-06-08 20:01:57 -0600

    Added comments to pynumbers.py

--- portfolio_project/Portfolio Project/pynumbers.py
+++ portfolio_project/Portfolio Project/pynumbers.py
@@ -6,7 +6,7 @@ import time
 # Note that this may take around 5 minutes to generate 1 billion rows
 file = open("./hugefile1.txt", "w")
 start_time = time.time()
-for i in range(1_000):
+for i in range(1_000_000_000):
     random_number = random.randint(1,100000)
     file.write(str(random_number))
     file.write("\n")
