228ae7a04bf4d75c2a680148f841e2a4354ff1bf
AFaurot <aaron.faurot@csuglobal.edu>
2025-06-08 20:01:33 -0600

    Added comments to pynumbers.py

--- portfolio_project/Portfolio Project/pynumbers.py
+++ portfolio_project/Portfolio Project/pynumbers.py
@@ -4,9 +4,9 @@ import time
 # simple script to generate a file - For the portfolio project it can be run twice
 # Name one file hugefile1.txt and the other file hugefile2.txt
 # Note that this may take around 5 minutes to generate 1 billion rows
-file = open("../hugefile1.txt", "w")
+file = open("./hugefile1.txt", "w")
 start_time = time.time()
-for i in range(1_000_000_000):
+for i in range(1_000):
     random_number = random.randint(1,100000)
     file.write(str(random_number))
     file.write("\n")
